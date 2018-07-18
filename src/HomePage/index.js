f9769c03d3796c1b0d712c88b21925217cad04cb
Judahesses <Judahesses@gmail.com>
2018-07-18 10:54:02 -0400

    HomePage tech setup

--- src/HomePage/index.js
+++ src/HomePage/index.js
@@ -6,34 +6,9 @@ class HomePage extends Component {
 
  render() {
    return(
-     <div className="top">
-     <div className="tech">
-      <Router>
-     <Link to="/technology">Technology</Link>
-     </Router>
-     </div>
-     <div className="business">
-     <Router>
-     <Link to="/business">Business</Link>
-     </Router>
-     </div>
-     <div className="politics">
-     <Router>
-     <Link to="/politics">Politics</Link>
-     </Router>
-     </div>
-     <div className="sports">
-     <Router>
-     <Link to="/sports">Sports</Link>
-     </Router>
-     </div>
-     <div className="culture">
-     <Router>
-     <Link to="/culture">Culture</Link>
-     </Router>
-     </div>
-    
-     </div>
+      <div className="categories">
+        <Link to="/technology">technology</Link>
+      </div>
    );
  }
 }
