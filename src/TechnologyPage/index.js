5cf4bbcc4f6db8dd3fdc667e30042c6a2fa56b5d
Judahesses <Judahesses@gmail.com>
2018-07-18 09:14:35 -0400

    Basic TechnologyPage

---
+++ src/TechnologyPage/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class TechnologyPage extends Component {
+  render() {
+    return <div className="TechnologyPage">Hello World</div>;
+  }
+}
+
+export default TechnologyPage;
