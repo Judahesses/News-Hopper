1f065816c1f756bce4cc9152b803843c0a2f8394
Judahesses <Judahesses@gmail.com>
2018-07-18 10:58:52 -0400

    Getting tech link to properly redirect to tech component on homepage

--- src/App/index.js
+++ src/App/index.js
@@ -1,43 +1,23 @@
 import React, { Component } from "react";
 import "./style.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import TechnologyPage from '../TechnologyPage';
+import HomePage from "../HomePage";
 
-class HomePage extends Component {
-
+class App extends Component {
  render() {
-   return(
-     <div className="top">
-     <div className="tech">
-      <Router>
-     <Link to="/technology">Technology</Link>
-     </Router>
-     </div>
-     <div className="business">
-     <Router>
-     <Link to="/business">Business</Link>
-     </Router>
-     </div>
-     <div className="politics">
-     <Router>
-     <Link to="/politics">Politics</Link>
-     </Router>
-     </div>
-     <div className="sports">
-     <Router>
-     <Link to="/sports">Sports</Link>
-     </Router>
-     </div>
-     <div className="culture">
-     <Router>
-     <Link to="/culture">Culture</Link>
-     </Router>
-     </div>
-    
-     </div>
+   return( 
+    <Router>
+      <div className="App">
+        <nav className='nav'>
+          <Link to='/'>Home</Link>
+        </nav>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/technology' component={TechnologyPage} />
+      </div>
+    </Router>
    );
  }
 }
 
-export default HomePage;
-
-
+export default App;
