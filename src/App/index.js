1cc8523c7f6bef1c359a3ddf5ac25705c1d9feab
Judahesses <Judahesses@gmail.com>
2018-07-19 17:54:57 -0400

    App component contains all category routes and components

--- src/App/index.js
+++ src/App/index.js
@@ -2,6 +2,12 @@ import React, { Component } from "react";
 import "./style.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import TechnologyPage from '../TechnologyPage';
+import BusinessPage from '../BusinessPage';
+import PoliticsPage from '../PoliticsPage';
+import EntertainmentPage from '../EntertainmentPage';
+import SportsPage from '../SportsPage';
+import SciencePage from '../SciencePage';
+import HealthPage from '../HealthPage';
 import HomePage from "../HomePage";
 
 class App extends Component {
@@ -14,6 +20,12 @@ class App extends Component {
         </nav>
         <Route exact path='/' component={HomePage} />
         <Route path='/technology' component={TechnologyPage} />
+        <Route path='/business' component={BusinessPage} />
+        <Route path='/politics' component={PoliticsPage} />
+        <Route path='/entertainment' component={EntertainmentPage} />
+        <Route path='/sports' component={SportsPage} />
+        <Route path='/science' component={SciencePage} />
+        <Route path='/health' component={HealthPage} />
       </div>
     </Router>
    );
