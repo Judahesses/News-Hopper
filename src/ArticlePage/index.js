ad51df8f53220a91e51614343050815468e17dcd
Thomas McKenna1 <tmckenna1@babson.edu>
2018-07-20 19:46:56 -0400

    works

--- src/ArticlePage/index.js
+++ src/ArticlePage/index.js
@@ -5,22 +5,22 @@ import moment from 'moment';
 
 class ArticlePage extends Component {
     constructor(props) {
-        super(props);
-    
-        this.state = {
-          businessNews: []
-        }
-      }
-    //   componentDidMount(){
-    //     fetch('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=a78e0b2dba4b489eb46d9605ed082d65')
-    //     .then(response => response.json())
-    //     .then(data => {
-    //       console.log(data.articles)
-    //       this.setState({
-    //         businessNews: data.articles[0]
-    //     })
-    //   })
-    //   }
+    super(props);
+
+    this.state = {
+      businessNews: []
+    }
+  }
+  componentDidMount(){
+    fetch('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=a78e0b2dba4b489eb46d9605ed082d65')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.articles)
+      this.setState({
+        businessNews: data.articles
+    })
+  })
+  }
     render() {
       return( 
         <div className="SingleArticle">
