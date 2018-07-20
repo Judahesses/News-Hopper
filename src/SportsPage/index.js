0848424d4beafeed3ac74923a225ceb74e928733
Judahesses <Judahesses@gmail.com>
2018-07-20 02:54:45 -0400

    Styling, and added image, time, npm package called moment to make time look better, and description

--- src/SportsPage/index.js
+++ src/SportsPage/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./style.css";
+import moment from 'moment'
 
 class SportsPage extends Component {
   constructor(props) {
@@ -15,16 +16,26 @@ class SportsPage extends Component {
     .then(data => {
       console.log(data.articles)
       this.setState({
-         sportsNews: data.articles
+        sportsNews: data.articles
     })
   })
   }
   render() {
     return (
-      <div> 
-        {this.state.sportsNews.map(article => 
-         <h1>{article.title} <a href={article.url} target="_blank">{article.url}</a></h1>   
-      )}
+      <div>
+        <h2 className='page-title'>Sports</h2>
+          <div className='container'> 
+            {this.state.sportsNews.map(article => 
+             <div className='business-news'>
+                <a href={article.url} target="_blank"><h2 className='title'>{article.title}</h2></a>
+                <h4 className='time'>{article.publishedAt = moment(article.publishedAt).format('LL')}</h4>
+                <div className='img-description'>
+                  <a href={article.url} target="_blank"><img className='img' src={article.urlToImage} alt='' /></a>
+                  <h3 className='description'>{article.description}</h3>
+                </div>
+              </div>
+            )}
+          </div>
       </div>
     )
   }
