947b2d79066db9ac277e737b9db37bd725abdb82
Judahesses <Judahesses@gmail.com>
2018-07-20 02:31:30 -0400

    updated technology page with styling

--- src/TechnologyPage/index.js
+++ src/TechnologyPage/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import "./style.css";
+import moment from 'moment'
 
 class TechnologyPage extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      techNews: []
+      technologyNews: []
     }
   }
   componentDidMount(){
@@ -15,16 +16,26 @@ class TechnologyPage extends Component {
     .then(data => {
       console.log(data.articles)
       this.setState({
-        techNews: data.articles
+        technologyNews: data.articles
     })
   })
   }
   render() {
     return (
-      <div> 
-        {this.state.techNews.map(article => 
-         <h1>{article.title} <a href={article.url} target="_blank">{article.url}</a></h1>   
-      )}
+      <div>
+        <h2 className='page-title'>Technology</h2>
+          <div className='container'> 
+            {this.state.technologyNews.map(article => 
+             <div className='business-news'>
+                <a href={article.url} target="_blank"><h2 className='title'>{article.title}</h2></a>
+                <h4 className='time'>{article.publishedAt = moment(article.publishedAt).format('LL')}</h4>
+                <div className='img-description'>
+                  <a href={article.url} target="_blank"><img className='img' src={article.urlToImage} alt='' /></a>
+                  <h3 className='description'>{article.description}</h3>
+                </div>
+              </div>
+            )}
+          </div>
       </div>
     )
   }
