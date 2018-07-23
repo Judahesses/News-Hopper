5d2fde84bd3e3ba40f6923b04c47861ab9e80914
Thomas McKenna1 <tmckenna1@babson.edu>
2018-07-23 11:13:02 -0400

    more work on bookmark

--- src/BookmarkPage/index.js
+++ src/BookmarkPage/index.js
@@ -1,19 +1,54 @@
 import React, { Component } from "react";
 import "./style.css";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import moment from 'moment';
+import { BrowserRouter as Router,
+  Route, Link } from 'react-router-dom';
 
-class BookMarkPage extends Component {
+class BookmarkPage extends Component {
+   constructor(props) {
+      super(props);
 
-    render() {
-      return(
-        <div className='Bookmark'>
-          <h1 className='app-title'>Bookmark</h1>
-           <div className="articles">
-             
-           </div> 
-         </div> 
-       );
-     }
+      this.state= {
+         id: 0,
+         title: '',
+         urlToImage: '',
+         author: '',
+         publishedAt: '',
+         description: '',
+         url: ''
+      }
    }
-   
-   export default BookMarkPage;
+   componentDidMount(){
+      let id = this.props.match.params.id;
+      console.log(this.props);
+      fetch(`/video-games/${id}.json`)
+      .then(response => response.json())
+      .then(article => {
+         this.setState({
+            id: article.id,
+            title: article.title,
+            urlToImage: article.urlToImage,
+            author: article.author,
+            publishedAt: article.publishedAt,
+            description: article.description,
+            url: article.url
+         })
+      })
+   }
+   render() {
+    return (
+      <div className="article">
+         <p className="title">{this.state.title}</p>
+         <div className="frame">
+            <p className="data">{moment(this.state.data).format('LL')}</p>
+            <img className="image_url" src={this.state.urlToImage} />
+            <p className="blurb">{this.state.description}</p>
+         </div>
+      </div>
+    );
+  }
+}
+
+moment().format('LL');
+
+export default BookmarkPage;
