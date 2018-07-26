a9f937e4c2d662231eba96d3f30d5aa20ac046ee
Judahesses <Judahesses@gmail.com>
2018-07-26 18:26:34 -0400

    connections.js

---
+++ database/connections.js
@@ -0,0 +1,27 @@
+const promise = require("bluebird");
+const monitor = require("pg-monitor");
+
+let initOptions = {};
+
+// Display better error stack traces in development.
+if (process.NODE_ENV !== "production") {
+  promise.config({
+    longStackTraces: true
+  });
+  initOptions = {
+    promiseLib: promise
+  };
+}
+
+// attach to all events at once;
+monitor.attach(initOptions, ["query", "error"]);
+
+// Import pg-promise and initialize the library with an empty object.
+const pgp = require("pg-promise")(initOptions);
+
+const connectionUrl = process.env.DATABASE_URL;
+
+// Creating a new database connection with the provided configuration.
+const db = pgp(connectionUrl);
+
+module.exports = db;
