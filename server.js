6711996c065b4370c60be27634e34dbbf78bb696
Judahesses <Judahesses@gmail.com>
2018-08-09 19:20:39 -0400

    proxy

--- server.js
+++ server.js
@@ -1,26 +1,25 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const Post = require('./models/Post');
+const express = require("express");
+const bodyParser = require("body-parser");
+const Post = require("./models/Post");
 
 const app = express();
 
 // Static hosting for built files
 app.use("/", express.static("./build/"));
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 4567;
 
-app.get('/reviews.json', (request, response) => {
-    Post.all()
-    .then(posts => {
-      response.json({posts})
-    })
-})
+app.get("/reviews.json", (request, response) => {
+  Post.all().then(posts => {
+    response.json({ posts });
+  });
+});
 
-app.post('/posts.json', (request, response) => {
+app.post("/posts.json", (request, response) => {
   const newPost = {
     author: request.body.author,
     stars: request.body.stars,
@@ -33,24 +32,22 @@ app.post('/posts.json', (request, response) => {
   });
 });
 
-app.get('/posts/:id.json', (request, response) => {
+app.get("/posts/:id.json", (request, response) => {
   let id = request.params.id;
-  Post.findById(id)
-  .then(post => {
+  Post.findById(id).then(post => {
     response.json(post);
   });
-})
+});
 
-app.delete('/posts/:id.json', (request, response) => {
-  const id = request.params.id
-  Post.delete(id)
-    .then(post => {
-      response.json(post);
-    })
-})
+app.delete("/posts/:id.json", (request, response) => {
+  const id = request.params.id;
+  Post.delete(id).then(post => {
+    response.json(post);
+  });
+});
 
-app.put('/posts/:id.json', (request, response) => {
-  let id = request.params.id
+app.put("/posts/:id.json", (request, response) => {
+  let id = request.params.id;
   const newPost = {
     id: id,
     author: request.body.author,
@@ -58,11 +55,10 @@ app.put('/posts/:id.json', (request, response) => {
     title: request.body.title,
     body: request.body.body
   };
-  Post.update(newPost)
-    .then(post => {
+  Post.update(newPost).then(post => {
     response.json(post);
   });
-})
+});
 
 if (process.env.NODE_ENV == "production") {
   app.get("/*", function(request, response) {
--- src/CategoryPage/index.js
+++ src/CategoryPage/index.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
 import "./style.css";
-import moment from 'moment';
-import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom";
+import moment from "moment";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
 
 class CategoryPage extends Component {
   constructor(props) {
@@ -9,52 +14,90 @@ class CategoryPage extends Component {
 
     this.state = {
       categoryNews: [],
-      categoryName: ''
-    }
+      categoryName: ""
+    };
   }
-  componentDidMount(){
+  componentDidMount() {
     // this grabs the path name, deletes the '/'
     let category = window.location.pathname.slice(1);
     // category (path name - '/') is inserted in the category=""
-    fetch(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${process.env.REACT_APP_APIKEY}`)
-    .then(response => response.json())
+    fetch(
+      `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${
+        process.env.REACT_APP_APIKEY
+      }`
+    )
+      .then(response => response.json())
       .then(data => {
-        console.log(data.articles)
+        console.log(data.articles);
         this.setState({
           categoryNews: data.articles,
           categoryName: category
-      })
-    })
+        });
+      });
   }
   render() {
     return (
-      <div className='CategoryPage'>
-      <nav className='nav'>
-          <p><Link className='home' to='/'>Home</Link></p>
-            <nav className='nav-categories'>
-              <Link to="/business" className="business-link">Business</Link>
-              <Link to="/technology" className="technology-link">Technology</Link>
-              <Link to="/politics" className="politics-link">Politics</Link>
-              <Link to="/entertainment" className="entertainment-link">Entertainment</Link>
-              <Link to="/sports" className="sports-link">Sports</Link>
-              <Link to="/science" className="science-link">Science</Link>
-            </nav>
+      <div className="CategoryPage">
+        <nav className="nav">
+          <p>
+            <Link className="home" to="/">
+              Home
+            </Link>
+          </p>
+          <nav className="nav-categories">
+            <Link to="/business" className="business-link">
+              Business
+            </Link>
+            <Link to="/technology" className="technology-link">
+              Technology
+            </Link>
+            <Link to="/politics" className="politics-link">
+              Politics
+            </Link>
+            <Link to="/entertainment" className="entertainment-link">
+              Entertainment
+            </Link>
+            <Link to="/sports" className="sports-link">
+              Sports
+            </Link>
+            <Link to="/science" className="science-link">
+              Science
+            </Link>
+          </nav>
         </nav>
-        <h2 className='page-title'>{this.state.categoryName.toUpperCase()}</h2>
-          <div className='container'>
-            {this.state.categoryNews.map(article =>
-             <div className='category-news'>
-              <a href={article.url} target="_blank"><h2 className='title'>{article.title}</h2></a>
-                <h4 className='time'>{article.publishedAt = moment(article.publishedAt).format('LL')}</h4>
-                <div className='img-description'>
-                  <a href={article.url} target="_blank"><img className='img' src={article.urlToImage || 'http://www.bsmc.net.au/wp-content/uploads/No-image-available.jpg'} /></a>
-                  <h3 className='description'>{article.description || 'Sorry, no description available.'}</h3>
-                </div>
+        <h2 className="page-title">{this.state.categoryName.toUpperCase()}</h2>
+        <div className="container">
+          {this.state.categoryNews.map(article => (
+            <div className="category-news">
+              <a href={article.url} target="_blank">
+                <h2 className="title">{article.title}</h2>
+              </a>
+              <h4 className="time">
+                {
+                  (article.publishedAt = moment(article.publishedAt).format(
+                    "LL"
+                  ))
+                }
+              </h4>
+              <div className="img-description">
+                <a href={article.url} target="_blank">
+                  <img
+                    className="img"
+                    src={
+                      article.urlToImage ||
+                      "http://www.bsmc.net.au/wp-content/uploads/No-image-available.jpg"
+                    }
+                  />
+                </a>
+                <h3 className="description">
+                  {article.description || "Sorry, no description available."}
+                </h3>
               </div>
-            )}
-          </div>
+            </div>
+          ))}
+        </div>
       </div>
-    )
+    );
   }
 }
 
